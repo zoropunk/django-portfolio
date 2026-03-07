e0f7ac7af2e3dba2ca47a7909fbc0bb6442be0b7
cyberzoro <raghavagarwal9755@gmail.com>
2026-03-07 11:47:57 +0530

    first commit

---
+++ portfolio/Base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=40)
+    email= models.EmailField(max_length=40)
+    content= models.TextField(max_length=400)
+    number=models.CharField(max_length=13)
---
+++ portfolio/Base/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from Base import models
+from Base.models import Contact
+
+# Create your views here.
+# def home(request):
+#     return render(request,'home.html')
+def contact(request):
+
+    if request.method == "POST":
+        print("POST request")
+
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        content = request.POST.get('content', '')
+        number = request.POST.get('number', '')
+
+        print(name, email, number, content)
+
+        if not (2 <= len(name) <= 30):
+            messages.error(request, "Name must be between 2 and 30 characters")
+            return render(request, "home.html")
+
+        if not (5 <= len(email) <= 50):
+            messages.error(request, "Invalid email")
+            return render(request, "home.html")
+
+        if not (3 <= len(number) <= 13):
+            messages.error(request, "Invalid number")
+            return render(request, "home.html")
+
+        ins = Contact(
+            name=name,
+            email=email,
+            number=number,
+            content=content
+        )
+
+        ins.save()
+
+        messages.success(request, "Thank you! Your message has been saved.")
+
+        print("Data saved to database")
+
+    return render(request, "home.html")
