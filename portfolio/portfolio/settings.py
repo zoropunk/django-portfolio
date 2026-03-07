c0800032b7c6ecc1a63c12362d797817880e51c8
cyberzoro <raghavagarwal9755@gmail.com>
2026-03-07 12:28:03 +0530

    fix static url

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
